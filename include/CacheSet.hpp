292857b8137eb290be3bcd6c6d3076cbf3cee76c
Srichari <121370343+Srichari@users.noreply.github.com>
2025-04-30 23:49:06 +0530

    Update CacheSet.hpp

--- include/CacheSet.hpp
+++ include/CacheSet.hpp
@@ -1,1 +1,18 @@
 
+#ifndef CACHESET_HPP
+#define CACHESET_HPP
+
+#include "CacheLine.hpp"
+#include <vector>
+
+class CacheSet {
+public:
+    std::vector<CacheLine> lines;
+    int E;
+
+    CacheSet(int E);
+    int findLine(uint32_t tag);
+    int getLRUVictim();
+};
+
+#endif // CACHESET_HPP
