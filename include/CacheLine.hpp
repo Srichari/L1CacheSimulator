62a4026f9c3f300f23847177a8bda7688b6735db
Srichari <121370343+Srichari@users.noreply.github.com>
2025-04-30 23:48:40 +0530

    Update CacheLine.hpp

--- include/CacheLine.hpp
+++ include/CacheLine.hpp
@@ -1,1 +1,26 @@
 
+#ifndef CACHELINE_HPP
+#define CACHELINE_HPP
+
+#include <cstdint>
+
+enum MESIState {
+    INVALID,
+    SHARED,
+    EXCLUSIVE,
+    MODIFIED
+};
+
+class CacheLine {
+public:
+    bool valid;
+    bool dirty;
+    uint32_t tag;
+    MESIState state;
+    uint64_t lastUsedCycle;
+
+    CacheLine();
+    void invalidate();
+};
+
+#endif // CACHELINE_HPP
