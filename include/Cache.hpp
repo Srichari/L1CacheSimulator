4bf612bbdc3aef1fd68385dc74bba4f6b068bc48
Srichari <121370343+Srichari@users.noreply.github.com>
2025-04-30 23:53:49 +0530

    Update Cache.hpp

--- include/Cache.hpp
+++ include/Cache.hpp
@@ -1,1 +1,29 @@
+#ifndef CACHE_HPP
+#define CACHE_HPP
 
+#include "CacheSet.h"
+#include "Bus.hpp"
+#include <vector>
+
+class Bus;
+
+class Cache {
+public:
+    int s, E, b, coreId;
+    int S, B;
+    std::vector<CacheSet> sets;
+    Bus* bus;
+
+    int readCount = 0;
+    int writeCount = 0;
+    int missCount = 0;
+    int evictions = 0;
+    int writebacks = 0;
+
+    Cache(int s, int E, int b, int coreId, Bus* bus);
+    bool access(bool isWrite, unsigned long address);
+    void snoop(const BusRequest& req);
+    void printStats();
+};
+
+#endif
