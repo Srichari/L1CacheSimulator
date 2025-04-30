d33599eadeedf7ec1b0dd9611e2847f41bf621d4
Srichari <121370343+Srichari@users.noreply.github.com>
2025-04-30 23:49:45 +0530

    Update cache_line.cpp

--- src/cache_line.cpp
+++ src/cache_line.cpp
@@ -1,1 +1,16 @@
+#include "CacheLine.hpp"
+
+CacheLine::CacheLine() {
+    valid = false;
+    dirty = false;
+    tag = 0;
+    state = INVALID;
+    lastUsedCycle = 0;
+}
+
+void CacheLine::invalidate() {
+    valid = false;
+    dirty = false;
+    state = INVALID;
+}
 
